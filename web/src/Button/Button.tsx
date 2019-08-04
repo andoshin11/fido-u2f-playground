e5c09f0b2a1cd752adaf9c74924c400f6a1f23b1
andoshin11 <shinglish11@gmail.com>
2019-08-04 11:12:32 +0900

    Add u2f-api

--- web/src/Button/Button.tsx
+++ web/src/Button/Button.tsx
@@ -1,5 +1,8 @@
 import * as tsx from 'vue-tsx-support'
+import { isSupported } from 'u2f-api'
 import styles from './styles.css'
+import { APIClient } from '@/infra/network/APIClient'
+import { RequestRegister } from '@/infra/network/requests'
 
 export default tsx.component({
   name: 'Button',
@@ -10,8 +13,13 @@ export default tsx.component({
     }
   },
   methods: {
-    sayHi() {
+    async sayHi() {
       console.log('Say hi!')
+      const client = new APIClient()
+      const result = await client.request(new RequestRegister())
+      console.log(result)
+      const canUse = await isSupported()
+      console.log(canUse)
     }
   },
   render() {
